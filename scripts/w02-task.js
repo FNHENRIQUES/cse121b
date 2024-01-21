910f7152bcc5268ab18ce4725037ffa1992de903
FNHENRIQUES <nandoje@hotmail.com>
2024-01-21 00:20:17 -0300

    novo

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,21 +3,33 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
-
+const fullName = "Fernando Negreiros Henriques";
+const year = new Date();
+let currentYear = year.getFullYear();
+let profilePicture = ("images/perfil.png");
 
 /* Step 3 - Element Variables */
-
-
-
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.getElementById("img");
 
 
 /* Step 4 - Adding Content */
 
+nameElement.innerHTML = (`<strong>${fullName}</strong>`);
+yearElement.innerHTML = (`${currentYear}`);
+imageElement.setAttribute = ("src", `${profilePicture}`);
+imageElement.setAttribute = ("alt", `Profile image of ${fullName}`);
 
 
-
+/* Step 5 - Array */
+let favFoods = [lazanha, feijoada, pizza, potato];
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.shift()
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.pop
+foodElement.innerHTML += `<br>${favFoods}`;
 
 
 /* Step 5 - Array */
